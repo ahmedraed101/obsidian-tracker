fc00373d9ad0d1891a4effe433b1e9aa3b49cbbb
Ahmed Raed <ahmed.raed@vultara.com>
2025-06-14 18:44:21 +0300

    Sat Jun(06) 14 2025 06-44-21 PM

---
+++ track_vault.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+echo "Start track of $(pwd) at $(date)"
+
+LOG_DIR="$HOME/logs"
+mkdir -p "$LOG_DIR"
+
+gitstatus=$(git status --porcelain)
+echo -e "$gitstatus"
+
+if [[ -n $gitstatus ]]; then
+    commitmessage="$(date +'%a %b(%m) %d %Y %I-%M-%S %p')"
+    echo $commitmessage
+    
+    git add .
+    git commit -m "$commitmessage"
+    git push
+fi
+
+
+echo "End track of $(pwd) at $(date)"
